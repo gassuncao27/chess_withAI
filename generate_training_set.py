24428a752b40ae956df86e02de429586d55b8baf
gassuncao27 <gassuncao27@gmail.com>
2024-01-02 18:50:06 -0300

    train and state moves

--- generate_training_set.py
+++ generate_training_set.py
@@ -11,12 +11,11 @@ for fn in os.listdir("data"):
             game = chess.pgn.read_game(pgn)
         except Exception:
             break
-        value = {'1/2-1/2':0, '0-1':-1, '1-0':1}[game.headers["Result"]]
+        value = {'1/2-1/2': 0, '0-1': -1, '1-0': 1}[game.headers["Result"]]
         print(value)
         board = game.board()
         for i, move in enumerate(game.mainline_moves()):
             board.push(move)
             # TODO: extract the boards
-            print(value, State(board).serialize())
-        exit(0)
-#     break    
+            print(value, State(board).serialize()[:, :, 0])
+    # break
